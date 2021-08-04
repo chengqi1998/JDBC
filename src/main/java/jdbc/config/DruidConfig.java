6896bed4a80969ece92d1425335b54aaf424b5c1
yanchengqi <1494299450@qq.com>
2021-08-04 14:23:55 +0800

    德鲁伊1

--- src/main/java/jdbc/config/DruidConfig.java
+++ src/main/java/jdbc/config/DruidConfig.java
@@ -1,4 +1,36 @@
 package jdbc.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+
+@Configuration
 public class DruidConfig {
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.druid")
+    public DataSource druidDatasource(){
+        System.out.println("配置德鲁伊数据源");
+        return new DruidDataSource();
+    }
+    @Bean
+    public ServletRegistrationBean<StatViewServlet> statViewServlet(){
+        System.out.println("配置Bean");
+        ServletRegistrationBean<StatViewServlet> bean = new ServletRegistrationBean<>(new StatViewServlet(), "/druid/*");
+//        后台的账号密码
+        HashMap<String,String> initParameters=new HashMap<>();
+//        添加配置
+        initParameters.put("loginUsername","admin");
+        initParameters.put("loginPassword","admin");
+        initParameters.put("allow","");
+        bean.setInitParameters(initParameters);
+        System.out.println("配置Bean2");
+        return  bean;
+
+    }
 }
